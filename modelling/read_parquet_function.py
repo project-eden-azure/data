8637a7f455ab7e62ffcf9824761842c2ddfa37b9
Don-Li <yli877@aucklanduni.ac.nz>
2020-06-18 11:51:41 +1200

    Deleted old model stuff

--- modelling/read_parquet_function.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import os
-import pyarrow.parquet as pq
-
-def read_parquet(filename):
-    dataset = pq.read_table( filename ).to_pandas()
-    return dataset
